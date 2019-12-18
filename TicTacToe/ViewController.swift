8d1c0c2ca27c6acc9cda4d3cd16d63ddfdabd39a
JChowCH1102 <48242411+JChowCH1102@users.noreply.github.com>
2019-12-18 15:06:36 +0800

    first commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by Jason Chow on 18/12/2019.
+//  Copyright © 2019 Jason Chow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
